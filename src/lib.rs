76ceb40f7c83d68f3b978814d47c61feab928d23
DarumaDocker <DarumaDocker@gmail.com>
2023-07-10 14:53:40 +0800

    test new google-cloud-service

--- src/lib.rs
+++ src/lib.rs
@@ -12,20 +12,21 @@ pub async fn run() {
 }
 
 async fn handler(_qry: HashMap<String, Value>, body: Vec<u8>) {
-    // let co = chat::ChatOptions {
-    //     ..Default::default()
-    // };
+    // match text_detection::text_detection(String::from_utf8_lossy(&body).into_owned()).await {
+    let co = chat::ChatOptions {
+        ..Default::default()
+    };
 
-    // let his = chat::chat_history("test-chat-for-vertex", 0);
-    // log::debug!("{:?}", his);
+    let his = chat::chat_history("test-chat-for-vertex", 0);
+    log::debug!("{:?}", his);
 
-    // match chat::chat(
-    //     "test-chat-for-vertex",
-    //     String::from_utf8_lossy(&body).into_owned().as_str(),
-    //     &co,
-    // )
-
-    match text_detection::text_detection(String::from_utf8_lossy(&body).into_owned()).await {
+    match chat::chat(
+        "test-chat-for-vertex",
+        String::from_utf8_lossy(&body).into_owned().as_str(),
+        &co,
+    )
+    .await
+    {
         Ok(x) => {
             send_response(
                 200,
